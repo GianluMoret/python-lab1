57718474a3d464f51e12476aa72b9cbab7fd8752
GianluMoret <gianluca.moret@studenti.polito.it>
2018-03-19 15:16:35 +0100

    first exercises of lab1

---
+++ Ex_1.py
@@ -0,0 +1,8 @@
+print("Compute the sum between two numbers")
+
+print("Enter the first number:")
+n1=input()
+print("Enter the second number:")
+n2=input()
+n=int(n1)+int(n2)
+print("The sum is ",n)
